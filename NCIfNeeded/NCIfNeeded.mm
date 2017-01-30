c2a8fd684d0f8701e0452dc200b0086f967c0150
Jay Zuerndorfer <jzplusplus@gmail.com>
2017-01-29 19:53:59 -0800

    Re-written for iOS 10

--- NCIfNeeded/NCIfNeeded.mm
+++ NCIfNeeded/NCIfNeeded.mm
@@ -3,32 +3,53 @@
 
 
 
+#import <UIKit/UIKit.h>
+
+
 #include <logos/logos.h>
 #include <substrate.h>
-@class SBModeViewController; 
-static void (*_logos_orig$_ungrouped$SBModeViewController$hostWillPresent)(SBModeViewController*, SEL); static void _logos_method$_ungrouped$SBModeViewController$hostWillPresent(SBModeViewController*, SEL); 
+@class SBModeViewController; @class SBNotificationCenterWithSearchViewController; 
+static void (*_logos_orig$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting)(SBNotificationCenterWithSearchViewController*, SEL); static void _logos_method$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting(SBNotificationCenterWithSearchViewController*, SEL); static void (*_logos_orig$_ungrouped$SBModeViewController$hostWillPresent)(SBModeViewController*, SEL); static void _logos_method$_ungrouped$SBModeViewController$hostWillPresent(SBModeViewController*, SEL); 
 
-#line 5 "/Users/jzplusplus/Documents/jailbreak/NCIfNeeded/NCIfNeeded/NCIfNeeded.xm"
+#line 8 "/Users/jzplusplus/Documents/jailbreak/NCIfNeeded/NCIfNeeded/NCIfNeeded.xm"
+
+
+
+static void _logos_method$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting(SBNotificationCenterWithSearchViewController* self, SEL _cmd) {
+    _logos_orig$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting(self, _cmd);
+    
+    if([[[self notificationListViewController] sectionList] sectionCount] == 0)
+    {
+        [[self notificationAndTodayContainerView] scrollToPageAtIndex:0 animated:0];
+    }
+    else
+    {
+        [[self notificationAndTodayContainerView] scrollToPageAtIndex:1 animated:0];
+    }
+}
+
+
+
 
 
 
 static void _logos_method$_ungrouped$SBModeViewController$hostWillPresent(SBModeViewController* self, SEL _cmd) {
     int index = 0;
-
+    
     if([[self viewControllers] count] > 1)
     {
         if( [[[[self viewControllers] objectAtIndex:1] orderedSectionIDs] count] != 0 )
         {
             index = 1;
         }
-
+        
         [self setSelectedViewController:[[self viewControllers] objectAtIndex:index]];
     }
-
-	_logos_orig$_ungrouped$SBModeViewController$hostWillPresent(self, _cmd);
+    
+    _logos_orig$_ungrouped$SBModeViewController$hostWillPresent(self, _cmd);
 }
 
 
 static __attribute__((constructor)) void _logosLocalInit() {
-{Class _logos_class$_ungrouped$SBModeViewController = objc_getClass("SBModeViewController"); MSHookMessageEx(_logos_class$_ungrouped$SBModeViewController, @selector(hostWillPresent), (IMP)&_logos_method$_ungrouped$SBModeViewController$hostWillPresent, (IMP*)&_logos_orig$_ungrouped$SBModeViewController$hostWillPresent);} }
-#line 25 "/Users/jzplusplus/Documents/jailbreak/NCIfNeeded/NCIfNeeded/NCIfNeeded.xm"
+{Class _logos_class$_ungrouped$SBNotificationCenterWithSearchViewController = objc_getClass("SBNotificationCenterWithSearchViewController"); MSHookMessageEx(_logos_class$_ungrouped$SBNotificationCenterWithSearchViewController, @selector(willActivateHosting), (IMP)&_logos_method$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting, (IMP*)&_logos_orig$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting);Class _logos_class$_ungrouped$SBModeViewController = objc_getClass("SBModeViewController"); MSHookMessageEx(_logos_class$_ungrouped$SBModeViewController, @selector(hostWillPresent), (IMP)&_logos_method$_ungrouped$SBModeViewController$hostWillPresent, (IMP*)&_logos_orig$_ungrouped$SBModeViewController$hostWillPresent);} }
+#line 46 "/Users/jzplusplus/Documents/jailbreak/NCIfNeeded/NCIfNeeded/NCIfNeeded.xm"
--- NCIfNeeded/NCIfNeeded.xm
+++ NCIfNeeded/NCIfNeeded.xm
@@ -2,23 +2,44 @@
 // Logos by Dustin Howett
 // See http://iphonedevwiki.net/index.php/Logos
 
+#import <UIKit/UIKit.h>
+
+//iOS 10 and up
+%hook SBNotificationCenterWithSearchViewController
+
+- (void)willActivateHosting
+{
+    %orig;
+    
+    if([[[self notificationListViewController] sectionList] sectionCount] == 0)
+    {
+        [[self notificationAndTodayContainerView] scrollToPageAtIndex:0 animated:0];
+    }
+    else
+    {
+        [[self notificationAndTodayContainerView] scrollToPageAtIndex:1 animated:0];
+    }
+}
+%end
+
+//Pre-iOS 10
 %hook SBModeViewController
 
 - (void)hostWillPresent
 {
     int index = 0;
-
+    
     if([[self viewControllers] count] > 1)
     {
         if( [[[[self viewControllers] objectAtIndex:1] orderedSectionIDs] count] != 0 )
         {
             index = 1;
         }
-
+        
         [self setSelectedViewController:[[self viewControllers] objectAtIndex:index]];
     }
-
-	%orig;
+    
+    %orig;
 }
 
 %end
