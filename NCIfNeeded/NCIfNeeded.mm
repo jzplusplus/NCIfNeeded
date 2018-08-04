60d48c64dd15b10ff1d564e60d7d70217e7022fc
Jay Zuerndorfer <jzplusplus@gmail.com>
2018-08-04 13:35:17 -0700

    Updated for iOS 11

--- NCIfNeeded/NCIfNeeded.mm
+++ NCIfNeeded/NCIfNeeded.mm
@@ -6,18 +6,68 @@
 #import <UIKit/UIKit.h>
 
 
-#include <logos/logos.h>
 #include <substrate.h>
-@class SBModeViewController; @class SBNotificationCenterWithSearchViewController; 
-static void (*_logos_orig$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting)(SBNotificationCenterWithSearchViewController*, SEL); static void _logos_method$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting(SBNotificationCenterWithSearchViewController*, SEL); static void (*_logos_orig$_ungrouped$SBModeViewController$hostWillPresent)(SBModeViewController*, SEL); static void _logos_method$_ungrouped$SBModeViewController$hostWillPresent(SBModeViewController*, SEL); 
+#if defined(__clang__)
+#if __has_feature(objc_arc)
+#define _LOGOS_SELF_TYPE_NORMAL __unsafe_unretained
+#define _LOGOS_SELF_TYPE_INIT __attribute__((ns_consumed))
+#define _LOGOS_SELF_CONST const
+#define _LOGOS_RETURN_RETAINED __attribute__((ns_returns_retained))
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
+#else
+#define _LOGOS_SELF_TYPE_NORMAL
+#define _LOGOS_SELF_TYPE_INIT
+#define _LOGOS_SELF_CONST
+#define _LOGOS_RETURN_RETAINED
+#endif
 
-#line 8 "/Users/jzplusplus/Documents/jailbreak/NCIfNeeded/NCIfNeeded/NCIfNeeded.xm"
+@class SBNotificationCenterWithSearchViewController; @class SBDashBoardViewController; @class SBModeViewController; 
+
+
+#line 7 "/Users/jzplusplus/Documents/jailbreak/NCIfNeeded/NCIfNeeded/NCIfNeeded.xm"
+static void (*_logos_orig$iOS11$SBDashBoardViewController$viewWillAppear$)(_LOGOS_SELF_TYPE_NORMAL SBDashBoardViewController* _LOGOS_SELF_CONST, SEL, bool); static void _logos_method$iOS11$SBDashBoardViewController$viewWillAppear$(_LOGOS_SELF_TYPE_NORMAL SBDashBoardViewController* _LOGOS_SELF_CONST, SEL, bool); 
 
 
 
-static void _logos_method$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting(SBNotificationCenterWithSearchViewController* self, SEL _cmd) {
-    _logos_orig$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting(self, _cmd);
+static void _logos_method$iOS11$SBDashBoardViewController$viewWillAppear$(_LOGOS_SELF_TYPE_NORMAL SBDashBoardViewController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, bool b) {
+    _logos_orig$iOS11$SBDashBoardViewController$viewWillAppear$(self, _cmd, b);
     
+    if([self isMainPageVisible] && ![[[self mainPageContentViewController] combinedListViewController] hasContent])
+    {
+        [self activatePage:[self _indexOfTodayPage] animated:NO withCompletion:nil];
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+static void (*_logos_orig$iOS10$SBNotificationCenterWithSearchViewController$willActivateHosting)(_LOGOS_SELF_TYPE_NORMAL SBNotificationCenterWithSearchViewController* _LOGOS_SELF_CONST, SEL); static void _logos_method$iOS10$SBNotificationCenterWithSearchViewController$willActivateHosting(_LOGOS_SELF_TYPE_NORMAL SBNotificationCenterWithSearchViewController* _LOGOS_SELF_CONST, SEL); 
+
+
+
+static void _logos_method$iOS10$SBNotificationCenterWithSearchViewController$willActivateHosting(_LOGOS_SELF_TYPE_NORMAL SBNotificationCenterWithSearchViewController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd) {
+    _logos_orig$iOS10$SBNotificationCenterWithSearchViewController$willActivateHosting(self, _cmd);
+
     if([[[self notificationListViewController] sectionList] sectionCount] == 0)
     {
         [[self notificationAndTodayContainerView] scrollToPageAtIndex:0 animated:0];
@@ -30,26 +80,35 @@ static void _logos_method$_ungrouped$SBNotificationCenterWithSearchViewControlle
 
 
 
+static void (*_logos_orig$preiOS10$SBModeViewController$hostWillPresent)(_LOGOS_SELF_TYPE_NORMAL SBModeViewController* _LOGOS_SELF_CONST, SEL); static void _logos_method$preiOS10$SBModeViewController$hostWillPresent(_LOGOS_SELF_TYPE_NORMAL SBModeViewController* _LOGOS_SELF_CONST, SEL); 
 
 
 
-static void _logos_method$_ungrouped$SBModeViewController$hostWillPresent(SBModeViewController* self, SEL _cmd) {
+static void _logos_method$preiOS10$SBModeViewController$hostWillPresent(_LOGOS_SELF_TYPE_NORMAL SBModeViewController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd) {
     int index = 0;
-    
+
     if([[self viewControllers] count] > 1)
     {
         if( [[[[self viewControllers] objectAtIndex:1] orderedSectionIDs] count] != 0 )
         {
             index = 1;
         }
-        
+
         [self setSelectedViewController:[[self viewControllers] objectAtIndex:index]];
     }
-    
-    _logos_orig$_ungrouped$SBModeViewController$hostWillPresent(self, _cmd);
+
+    _logos_orig$preiOS10$SBModeViewController$hostWillPresent(self, _cmd);
 }
 
 
-static __attribute__((constructor)) void _logosLocalInit() {
-{Class _logos_class$_ungrouped$SBNotificationCenterWithSearchViewController = objc_getClass("SBNotificationCenterWithSearchViewController"); MSHookMessageEx(_logos_class$_ungrouped$SBNotificationCenterWithSearchViewController, @selector(willActivateHosting), (IMP)&_logos_method$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting, (IMP*)&_logos_orig$_ungrouped$SBNotificationCenterWithSearchViewController$willActivateHosting);Class _logos_class$_ungrouped$SBModeViewController = objc_getClass("SBModeViewController"); MSHookMessageEx(_logos_class$_ungrouped$SBModeViewController, @selector(hostWillPresent), (IMP)&_logos_method$_ungrouped$SBModeViewController$hostWillPresent, (IMP*)&_logos_orig$_ungrouped$SBModeViewController$hostWillPresent);} }
-#line 46 "/Users/jzplusplus/Documents/jailbreak/NCIfNeeded/NCIfNeeded/NCIfNeeded.xm"
+
+
+static __attribute__((constructor)) void _logosLocalCtor_d89c9af8(int __unused argc, char __unused **argv, char __unused **envp) {
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 11.0) {
+        {Class _logos_class$iOS11$SBDashBoardViewController = objc_getClass("SBDashBoardViewController"); MSHookMessageEx(_logos_class$iOS11$SBDashBoardViewController, @selector(viewWillAppear:), (IMP)&_logos_method$iOS11$SBDashBoardViewController$viewWillAppear$, (IMP*)&_logos_orig$iOS11$SBDashBoardViewController$viewWillAppear$);}
+    } else if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 10.0) {
+        {Class _logos_class$iOS10$SBNotificationCenterWithSearchViewController = objc_getClass("SBNotificationCenterWithSearchViewController"); MSHookMessageEx(_logos_class$iOS10$SBNotificationCenterWithSearchViewController, @selector(willActivateHosting), (IMP)&_logos_method$iOS10$SBNotificationCenterWithSearchViewController$willActivateHosting, (IMP*)&_logos_orig$iOS10$SBNotificationCenterWithSearchViewController$willActivateHosting);}
+    } else {
+        {Class _logos_class$preiOS10$SBModeViewController = objc_getClass("SBModeViewController"); MSHookMessageEx(_logos_class$preiOS10$SBModeViewController, @selector(hostWillPresent), (IMP)&_logos_method$preiOS10$SBModeViewController$hostWillPresent, (IMP*)&_logos_orig$preiOS10$SBModeViewController$hostWillPresent);}
+    }
+}
--- NCIfNeeded/NCIfNeeded.xm
+++ NCIfNeeded/NCIfNeeded.xm
@@ -4,13 +4,44 @@
 
 #import <UIKit/UIKit.h>
 
-//iOS 10 and up
+%group iOS11
+%hook SBDashBoardViewController
+
+- (void)viewWillAppear: (bool)b
+{
+    %orig(b);
+    
+    if([self isMainPageVisible] && ![[[self mainPageContentViewController] combinedListViewController] hasContent])
+    {
+        [self activatePage:[self _indexOfTodayPage] animated:NO withCompletion:nil];
+    }
+//    id mainCombined = [[self mainPageContentViewController] combinedListViewController];
+//    id ncCombined = MSHookIvar<id>(mainCombined, "_listViewController");
+//
+//    if([self isMainPageVisible])
+//    {
+//        if([[ncCombined notificationSectionList] sectionCount] == 0)
+//        {
+//            [self activatePage:[self _indexOfTodayPage] animated:NO withCompletion:nil];
+//        }
+//        else
+//        {
+//            [ncCombined forceNotificationHistoryRevealed:YES animated:YES];
+//        }
+//    }
+}
+
+%end
+%end
+
+
+%group iOS10
 %hook SBNotificationCenterWithSearchViewController
 
 - (void)willActivateHosting
 {
     %orig;
-    
+
     if([[[self notificationListViewController] sectionList] sectionCount] == 0)
     {
         [[self notificationAndTodayContainerView] scrollToPageAtIndex:0 animated:0];
@@ -21,25 +52,37 @@
     }
 }
 %end
+%end
 
-//Pre-iOS 10
+%group preiOS10
 %hook SBModeViewController
 
 - (void)hostWillPresent
 {
     int index = 0;
-    
+
     if([[self viewControllers] count] > 1)
     {
         if( [[[[self viewControllers] objectAtIndex:1] orderedSectionIDs] count] != 0 )
         {
             index = 1;
         }
-        
+
         [self setSelectedViewController:[[self viewControllers] objectAtIndex:index]];
     }
-    
+
     %orig;
 }
 
 %end
+%end
+
+%ctor {
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 11.0) {
+        %init(iOS11);
+    } else if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 10.0) {
+        %init(iOS10);
+    } else {
+        %init(preiOS10);
+    }
+}
